95661893b77bfdb5bb62591f126fcf8eeff8fd61
brissonkyle <98568756+brissonkyle@users.noreply.github.com>
2022-04-25 11:21:43 -0600

    Before Vuetify install

---
+++ src/stores/loginstore.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+
+export const UseLoginStore = defineStore('loginStore',{
+    state: () => {
+        return {
+            completedLogin : '',
+            loginFailed : 'You failed to login',
+        }
+    },
+    actions : {
+        loginApi(){
+            axios.request( { 
+                url: 'https://reqres.in/api/login',
+                method: 'POST',
+                params : {
+                    email : 'eve.holt@reqres.in',
+                    password : 'cityslicka'
+                },
+            }).then((response) => {
+                this.completedLogin = response;
+                console.log(response.data);
+            }).catch((error) => {
+                this.loginFailed = error;
+                console.log(this.loginFailed);
+            });
+        }
+    },
+    getters: {
+        
+    }
+})
---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Welcome to the Login Page</h1>
+        <h3>Email</h3>
+        <input type="text">
+        <h3>Password</h3>
+        <input type="text">
+        <div>
+            <button @click="loginApi">Login</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import {UseLoginStore} from '@/stores/loginstore'
+import { mapState } from 'pinia'
+    export default {
+        name : 'LoginPage',
+        computed : {
+
+            ...mapState(UseLoginStore , ['loginApi'])
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
